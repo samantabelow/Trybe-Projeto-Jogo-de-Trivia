b53ea258f59b9d5ce6d1472b9b5900a88ce891fa
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-12 19:03:14 -0300

    refatorando para uma pergunta por vez

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Question from './Question';
 // import actions from '../actions';
 
 class GamePage extends React.Component {
   constructor() {
     super();
     this.fetchGame = this.fetchGame.bind(this);
-    this.renderGame = this.renderGame.bind(this);
     // this.randomOptions = this.randomOptions.bind(this);
     this.state = {
       games: [],
+      loading: true,
     };
   }
 
   componentDidMount() {
     const { token } = this.props;
     // const recoveredToken = localStorage.getItem('token');
-    console.log(token);
     this.fetchGame(token);
   }
 
@@ -25,58 +25,35 @@ class GamePage extends React.Component {
     try {
       const fetchAPI = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
       const gameInfo = await fetchAPI.json();
+      console.log(gameInfo);
       this.setState({
         games: gameInfo.results,
+        loading: false,
       });
     } catch (error) {
       return error;
     }
   }
 
-  // randomOptions(correctAnswer, incorrectAnswers) {
-  //   return (
-  //     <div>
-  //       <button data-testid="correct-answer">{correctAnswer}</button>
-  //       {incorrectAnswers.map((answer, index) => <button data-testid={`wrong-answer-${index}`}>{answer}</button>)}
-  //     </div>)
-  // }
-
-  renderGame(game, index) {
+  render() {
+    const { games, loading } = this.state;
+    if (loading) {
+      return <p>Loading...</p>;
+    }
     const {
       category,
       question,
       correct_answer: correctAnswer,
       incorrect_answers: incorrectAnswers,
-    } = game;
+    } = games[0];
     return (
       <div>
-        <p key={ `category${index}` } data-testid="question-text">{question}</p>
-        <p key={ `question${index}` } data-testid="question-category">{category}</p>
-        <button
-          type="button"
-          key={ `right${index}` }
-          data-testid="correct-answer"
-        >
-          {correctAnswer}
-        </button>
-        {incorrectAnswers.map((answer, order) => (
-          <button
-            type="button"
-            key={ `wrong${order}` }
-            data-testid={ `wrong-answer-${order}` }
-          >
-            {answer}
-          </button>))}
-        {/* {randomOptions(correct_answer, incorrect_answers)} */}
-      </div>
-    );
-  }
-
-  render() {
-    const { games } = this.state;
-    return (
-      <div>
-        {games.map((game, index) => this.renderGame(game, index))}
+        <Question
+          category={ category }
+          question={ question }
+          correctAnswer={ correctAnswer }
+          incorrectAnswers={ incorrectAnswers }
+        />
       </div>
     );
   }
