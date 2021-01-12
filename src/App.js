a3308cd56b6e2d2b8863dded1996d370b7ecfd65
nyxnil <nsmnilsonn@gmail.com>
2021-01-12 17:37:14 -0300

    add botao de configuracao e a pagina

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import GamePage from './components/GamePage';
+import Settings from './components/Settings';
 
 export default function App() {
   return (
     <div>
       <Switch>
-        <Route path="/gamepage" component={ GamePage } />
-        <Route exact path="/" component={ Login } />
+        <Route path="/gamepage" component={GamePage} />
+        <Route exact path="/" component={Login} />
+        <Route path="/settings" component={Settings} />
       </Switch>
     </div>
   );
---
+++ src/components/Settings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Config = props => (
+  <div>
+    <h1 data-testid='settings-title'></h1>
+  </div>
+)
+
+export default Config;
